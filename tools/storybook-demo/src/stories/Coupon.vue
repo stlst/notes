700c00a017516add1a179ffd0b2603a18ec3ad96
stlst <stlst@foxmail.com>
2022-03-30 00:59:13 +0800

    feat: add storybook demo

---
+++ tools/storybook-demo/src/stories/Coupon.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>class coupon</div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue, Watch } from "vue-property-decorator";
+
+@Component
+export default class Coupon extends Vue {
+  @Prop({}) primary: boolean;
+
+  @Watch("primary")
+  handlePrimaryChange() {
+    console.log("===primary", this.primary);
+  }
+
+  created() {
+    console.log("===props", this.primary);
+  }
+}
+</script>
+
+<style scoped></style>
